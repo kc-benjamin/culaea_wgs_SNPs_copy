5ec1de991dfdfff6105834a423b8bb9302196cee
kc-benjamin <katherine.benjamin@uga.edu>
2025-08-15 12:43:23 -0400

    added directory info

--- 01_scripts/01_fastp.sh
+++ 01_scripts/01_fastp.sh
@@ -28,8 +28,8 @@ samp_num=$1
 sample_name=$(cut -f1 /home/kcb95328/culaea_wgs_SNPs_copy/02_info_files/datatable.txt | sed -n "${samp_num}p")
 
 fastp -w ${SLURM_CPUS_PER_TASK} \
-        -i $INDIR/$(cut -f12 02_info_files/datatable.txt | sed -n "${samp_num}p") \
-        -I $INDIR/$(cut -f13 02_info_files/datatable.txt | sed -n "${samp_num}p") \
+        -i $INDIR/$(cut -f12 /home/kcb95328/culaea_wgs_SNPs_copy/02_info_files/datatable.txt | sed -n "${samp_num}p") \
+        -I $INDIR/$(cut -f13 /home/kcb95328/culaea_wgs_SNPs_copy/02_info_files/datatable.txt | sed -n "${samp_num}p") \
         -o $OUTDIR/"$sample_name".R1.trimmed.fastq.gz \
         -O $OUTDIR/"$sample_name".R2.trimmed.fastq.gz \
         -j $OUTDIR/01_reports/"$sample_name".json \

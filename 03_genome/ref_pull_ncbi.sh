50a022fbdc0abdc96b6f93328d2a147126123475
kc-benjamin <katherine.benjamin@uga.edu>
2025-11-17 15:48:18 -0500

    added conda init bash

--- 03_genome/ref_pull_ncbi.sh
+++ 03_genome/ref_pull_ncbi.sh
@@ -9,10 +9,11 @@
 #SBATCH --job-name=ref_index
 #SBATCH --error=ref_pull_ncbi_%j.err
 
+conda init bash
 conda activate /home/kcb95328/miniconda3/envs/ncbi_datasets
 conda activate ncbi_datasets
     conda install -c conda-forge ncbi-datasets-cli
-    
+
 module load datasets/3.1.0-foss-2023a
 
 datasets download genome accession CASGFK000000000.1 --filename punpun-ref-genome.fa

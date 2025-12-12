7a5a3ea601170830427e2940dcd1c7e8b09428a4
kc-benjamin <katherine.benjamin@uga.edu>
2025-12-12 13:47:13 -0500

    created a script to make a reference genome index

---
+++ 01_scripts/0y_genome_index_creation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --partition=batch  
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=10G
+#SBATCH --time=0-24:00:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --array=0-96
+
+module load BWA/0.7.18-GCCcore-13.3.0
+
+#location variables
+GENOMEFOLDER="03_genome"
+GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fa | xargs -n 1 basename) #changed to brook genome
+GENOME_FULL="$GENOMEFOLDER/$GENOME"
+
+bwa index [-a bwtsw|is] $GENOME_FULL bwa-index-brook

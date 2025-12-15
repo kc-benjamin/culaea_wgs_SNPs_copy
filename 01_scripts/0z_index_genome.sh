b44adb1818871f5dfc87265bf2a2cb7cc7429262
kc-benjamin <katherine.benjamin@uga.edu>
2025-12-15 11:50:52 -0500

    rewrote log files directory

--- 01_scripts/0z_index_genome.sh
+++ 01_scripts/0z_index_genome.sh
@@ -8,8 +8,8 @@
 #SBATCH --time=0-24:00:00
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=98_log_files/0z_genome_%j.out
-#SBATCH --error=98_log_files/0z_genome_%j.err
+#SBATCH --output=/scratch/kcb95328/Mee-Culaea-WGS/98_log_files/0z_genome_%j.out
+#SBATCH --error=/scratch/kcb95328/Mee-Culaea-WGS/98_log_files/0z_genome_%j.err
 
 ml BWA/0.7.18-GCCcore-13.3.0
 bwa index -p brook_genome_hap1_v1.fa -a is brook_genome_hap1_v1.fa

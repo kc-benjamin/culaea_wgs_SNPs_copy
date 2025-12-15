29f5eb1bf1069125682da9e5cc54e06c4669c3a5
kc-benjamin <katherine.benjamin@uga.edu>
2025-12-15 11:14:01 -0500

    added error output, changed names to make them easier to find

--- 01_scripts/0z_index_genome.sh
+++ 01_scripts/0z_index_genome.sh
@@ -8,7 +8,8 @@
 #SBATCH --time=0-24:00:00
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --output=98_log_files/0z_genome_%j.out
+#SBATCH --error=98_log_files/0z_genome_%j.err
 
 ml BWA/0.7.18-GCCcore-13.3.0
 bwa index -p brook_genome_hap1_v1.fa -a is brook_genome_hap1_v1.fa

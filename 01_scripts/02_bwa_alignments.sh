63bcd09ab130f029f06bc9d4e6292ee56e78569d
kc-benjamin <katherine.benjamin@uga.edu>
2026-01-20 19:53:47 -0500

    changed back to .fa

--- 01_scripts/02_bwa_alignments.sh
+++ 01_scripts/02_bwa_alignments.sh
@@ -18,7 +18,7 @@ module load BWA/0.7.18-GCCcore-13.3.0 SAMtools/1.21-GCC-13.3.0
 
 # Global variables
 GENOMEFOLDER="/scratch/kcb95328/Mee-Culaea-WGS/03_genome"
-GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fasta | xargs -n 1 basename) #changed to brook genome
+GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fa | xargs -n 1 basename) #changed to brook genome
 GENOME_FULL="$GENOMEFOLDER/$GENOME"
 ##INDGENOME="${GENOME}.fai"
 RAWDATAFOLDER="05_trimmed_data"

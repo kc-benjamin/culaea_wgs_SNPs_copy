c4620696e3638cc76fc61074e2c8575e39824b1b
kc-benjamin <katherine.benjamin@uga.edu>
2026-01-21 11:15:02 -0500

    forgot to change array back to 1

--- 01_scripts/02_bwa_alignments.sh
+++ 01_scripts/02_bwa_alignments.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j_.out
 #SBATCH --error=98_log_files/%x_%j_.err
-#SBATCH --array=0-96
+#SBATCH --array=1-1
 
 #PREFIX=$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt)
 # Load needed modules

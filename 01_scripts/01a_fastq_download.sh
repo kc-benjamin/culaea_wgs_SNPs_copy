d65ff96653de1ef6a3d64dbfe5fc94d1a3eda007
kc-benjamin <katherine.benjamin@uga.edu>
2026-01-27 10:58:40 -0500

    trying prefetch

--- 01_scripts/01a_fastq_download.sh
+++ 01_scripts/01a_fastq_download.sh
@@ -8,8 +8,8 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=98_log_files/%a_%x.out
-#SBATCH --error=98_log_files/%a_%x.err
+#SBATCH --output=98_log_files/%j_%x.out
+#SBATCH --error=98_log_files/%j_%x.err
 #SBATCH --array=1-97
 
 #Go to correct directory
@@ -17,6 +17,7 @@ cd $SLURM_SUBMIT_DIR
 #load package
 ml SRA-Toolkit/3.0.3-gompi-2022a
 #get accession number
-name=$(sed -n "$(($SLURM_ARRAY_TASK_ID))p" /scratch/kcb95328/Mee-Culaea-WGS/02_info_files/SRR_Acc_List_ML.txt)
+name=$(sed -n "$(($SLURM_ARRAY_TASK_ID))p" 02_info_files/SRR_Acc_List_ML.txt)
 #fastq conversion
-fasterq-dump $name --split-spot
+prefetch $name && fastq-dump $name --split-files -O /scratch/kcb95328/Mee-Culaea-WGS/04_raw_data/
+#fasterq-dump $name --split-spot

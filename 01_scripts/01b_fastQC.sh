f6c335d7bc37a1ce92c43a241bed8c9c33421358
kc-benjamin <katherine.benjamin@uga.edu>
2026-01-27 16:24:30 -0500

    added a script for fastqc

---
+++ 01_scripts/01b_fastQC.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=10G
+#SBATCH --time=0-00:30:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=98_log_files/%a_%x.out
+#SBATCH --error=98_log_files/%a_%x.err
+#SBATCH --array=1-97
+
+module load FastQC/0.12.1-Java-11
+
+fastqc -o 02_fastQC \
+    -t ${SLURM_CPUS_PER_TASK} 04_raw_data/$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt)_1.fastq \
+    04_raw_data/$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt)_2.fastq

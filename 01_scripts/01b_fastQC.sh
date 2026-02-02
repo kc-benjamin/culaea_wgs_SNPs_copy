76f440bca13aec41e3a9b57c8bf8ab24bd312bea
kc-benjamin <katherine.benjamin@uga.edu>
2026-02-02 12:49:29 -0500

    fixed directories

--- 01_scripts/01b_fastQC.sh
+++ 01_scripts/01b_fastQC.sh
@@ -16,5 +16,5 @@ module load FastQC/0.12.1-Java-11
 
 fastqc -o /scratch/kcb95328/Mee-Culaea-WGS/05_trimmed_data/03_fastQC_trimmed_files \
     -t ${SLURM_CPUS_PER_TASK} \
-    04_raw_data/$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt).R1.trimmed.fastq.gz \
-    04_raw_data/$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt).R2.trimmed.fastq.gz
+    05_trimmed_data/$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt).R1.trimmed.fastq.gz \
+    05_trimmed_data/$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt).R2.trimmed.fastq.gz

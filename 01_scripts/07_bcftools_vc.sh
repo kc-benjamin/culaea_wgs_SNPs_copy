8d2052f17c5040855f8ea5a1df53a55a825e584c
kc-benjamin <katherine.benjamin@uga.edu>
2026-02-06 12:58:34 -0500

    changed to chromosome text file & brook genome

--- 01_scripts/07_bcftools_vc.sh
+++ 01_scripts/07_bcftools_vc.sh
@@ -9,23 +9,24 @@
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j.out
-#SBATCH --array=0-96
+#SBATCH --array=1-1
 
 # Load needed modules
-module gcc/9.3.0 bcftools/1.11
+module BCFtools/1.21-GCC-13.3.0
+#gcc/9.3.0
 
 # Global variables
 INFO="02_info_files"
 GENOMEFOLDER="03_genome"
-GENOME=$(ls -1 $GENOMEFOLDER/*fa | xargs -n 1 basename)
-VCF="./07_raw_VCFs"
-BAM="./02_info_files/bamfiles.txt"
-SAMPS="./02_info_files/sample_file.txt"
+GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fa | xargs -n 1 basename)
+VCF="07_raw_VCFs"
+BAM="02_info_files/bamfiles.txt"
+SAMPS="02_info_files/SRR_Acc_List_ML.txt" #why does this not split it by file?
 
 #Pass the chromosome number from the sbatch command
 chrom_num=$1
 
 # Fetch chromosome from the array
-CHROM=$(sed -n "${chrom_num}p" ./02_info_files/chromosomes.txt)
+CHROM=$(sed -n "${chrom_num}p" /02_info_files/brook_genome_hap1_v1_chromosomes.txt)
 
 bcftools mpileup -Ou -f $GENOMEFOLDER/$GENOME --bam-list $BAM -q 5 -r $CHROM -I -a FMT/AD | bcftools call -S $SAMPS -G - -f GQ -mv -Ov > $VCF/${CHROM}\.vcf

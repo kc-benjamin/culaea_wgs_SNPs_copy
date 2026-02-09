c2bcc31cdbcfc9f08341f9ca61e6c14a4f9334db
kc-benjamin <katherine.benjamin@uga.edu>
2026-02-09 16:44:37 -0500

    changed for AL

--- 01_scripts/01_fastp.sh
+++ 01_scripts/01_fastp.sh
@@ -10,15 +10,15 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%a_%x.out
 #SBATCH --error=98_log_files/%a_%x.err
-#SBATCH --array=1-1
+#SBATCH --array=1-119
 
 #PREFIX=$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt)
-#currently for Muir Lake
+#currently for Astotin Lake
 # Load up fastp
 module load fastp/0.23.4-GCC-13.2.0
 
 # Variables
-INDIR="04_raw_data/split-read-files"
+INDIR="04_raw_data"
 OUTDIR="05_trimmed_data"
 LOG="98_log_files"
 #mkdir $OUTDIR/01_reports
@@ -28,8 +28,7 @@ LOG="98_log_files"
 samp_num=$SLURM_ARRAY_TASK_ID
 
 #Pull sample name from the sample info
-#sample_name=$(cut -f1 02_info_files/SRR_Acc_List_ML.txt | sed -n "${samp_num}p")
-sample_name=SRR19221338
+sample_name=$(cut -f1 02_info_files/SRR_Acc_List_AL.txt | sed -n "${samp_num}p")
 
 fastp -w ${SLURM_CPUS_PER_TASK} --trim_poly_g --dedup \
         -i "$INDIR/${sample_name}_1.fastq.gz" \

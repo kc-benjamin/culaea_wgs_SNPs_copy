9510c1f04f5e0fc845d6ed303e36eb24a6d94a0f
kc-benjamin <katherine.benjamin@uga.edu>
2026-02-10 10:35:37 -0500

    w last commit

--- 01_scripts/07_bcftools_vc.sh
+++ 01_scripts/07_bcftools_vc.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j.out
 #SBATCH --error=98_log_files/%x_%j.err
-#SBATCH --array=1-97
+#SBATCH --array=1-23
 
 # Load needed modules
 module load BCFtools/1.21-GCC-13.3.0

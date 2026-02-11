e4617722fde3526b4140a3011ad0c02643d8dd78
kc-benjamin <katherine.benjamin@uga.edu>
2026-02-11 10:44:32 -0500

    specified output.vcf (renamed version of the weird file 07 was producing)

--- 01_scripts/08_concat.sh
+++ 01_scripts/08_concat.sh
@@ -9,11 +9,12 @@
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --error=98_log_files/%x_%j.err
 
 module load VCFtools/0.1.16-GCC-13.3.0 BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCcore-13.3.0
 #module intel/2020.1.217 
 
-vcf-concat $(ls -1 ./07_raw_VCFs/* | perl -pe 's/\n/ /g') > muir_snps.vcf
+vcf-concat $(ls -1 ./07_raw_VCFs/output.vcf | perl -pe 's/\n/ /g') > muir_snps.vcf
 
 bgzip muir_snps.vcf
 tabix -p vcf muir_snps.vcf.gz

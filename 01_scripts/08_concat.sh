13ce8e23c4007854c28e5c916adb01f40efce72d
kc-benjamin <katherine.benjamin@uga.edu>
2026-02-16 14:39:36 -0500

    fixing bug

--- 01_scripts/08_concat.sh
+++ 01_scripts/08_concat.sh
@@ -14,10 +14,10 @@
 module load VCFtools/0.1.16-GCC-13.3.0 BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCcore-13.3.0
 #module intel/2020.1.217 
 
-vcf-concat $(ls -1 ./07_raw_VCFs/output.vcf | perl -pe 's/\n/ /g') > muir_snps.vcf
+vcf-concat $(ls -1 /07_raw_VCFs/* | perl -pe 's/\n/ /g') > muir_snps.vcf
 
 bgzip muir_snps.vcf
-tabix -p vcf muir_snps.vcf.gz
+tabix -p -f vcf muir_snps.vcf.gz
 
 bcftools filter -e 'MQ < 30' muir_snps.vcf.gz -Oz > tmp.vcf.gz
 

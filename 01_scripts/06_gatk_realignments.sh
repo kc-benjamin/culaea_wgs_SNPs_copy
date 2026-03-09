209707a56e3036a2993463e84d7e4e8197a322ef
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-09 12:50:17 -0400

    added conda, commented-in the samtools index

--- 01_scripts/06_gatk_realignments.sh
+++ 01_scripts/06_gatk_realignments.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j.out
 #SBATCH --error=98_log_files/%x_%j.err
-#SBATCH --array=1-97
+#SBATCH --array=1-1
 
 # Copy script to log folder
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
@@ -18,6 +18,10 @@ SCRIPT=$0
 NAME=$(basename $0)
 LOG_FOLDER="98_log_files"
 cp "$SCRIPT" "$LOG_FOLDER"/"$TIMESTAMP"_"$NAME"
+
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh 
+conda activate /home/kcb95328/conda/envs/culaea_pkgs
 
 # Load needed modules
 #module load SAMtools/1.18-GCC-12.3.0
@@ -42,20 +46,20 @@ samp_num=$SLURM_ARRAY_TASK_ID
 # Fetch filename from the array
 sample_name=$(cut -f1 02_info_files/SRR_Acc_List_ML.txt | sed -n "${samp_num}p")
 
-file=${sample_name}.trimmed.fastq.gz.sorted.bam ##changed to the trimmed sorted file because i dedup-ed using fastp
+file=${sample_name}.dedup.bam ##changed to the trimmed sorted file because i dedup-ed using fastp
+#file=${sample_name}.dedup.bam #for files that went through the deduplication process (Astotin)
 
 echo "
      >>> Realigning TARGET for $file <<<
      "
 
 # Index the bam file First #what is this for? to make coordinates to point the script where in the genome each read belongs
-#samtools index $BAM/$file
+samtools index $BAM/$file
 #commented out because I am skipping script 04 and already did this
 
 # Now load modules
 #module purge
 #module load nixpkgs/16.09 #ensure that this is right#
-ml Java/1.8.0_241 GATK/3.8-1-Java-1.8.0_241
 
 # Realign
 java -jar $EBROOTGATK/GenomeAnalysisTK.jar \
@@ -79,4 +83,4 @@ echo ">>> Cleaning a bit...
 "
 echo "
 DONE! Check your files"
-conda deactivate
+#conda deactivate

ec54c2736bd9ad1af8e8c4a512c5ed6e0686c692
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-10 11:18:27 -0400

    changed to just run on sample 314

--- 01_scripts/06_gatk_realignments.sh
+++ 01_scripts/06_gatk_realignments.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j.out
 #SBATCH --error=98_log_files/%x_%j.err
-#SBATCH --array=2-98
+#SBATCH --array=1-1
 
 # Copy script to log folder
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
@@ -42,10 +42,11 @@ echo " >>> Realigning...
 samp_num=$SLURM_ARRAY_TASK_ID
 
 # Fetch filename from the array
-sample_name=$(cut -f1 02_info_files/SRR_Acc_List_ML.txt | sed -n "${samp_num}p")
+# sample_name=$(cut -f1 02_info_files/SRR_Acc_List_ML.txt | sed -n "${samp_num}p")
+sample_name=SRR19221314
 
-file=${sample_name}.dedup.bam ##changed to the trimmed sorted file because i dedup-ed using fastp
-#file=${sample_name}.dedup.bam #for files that went through the deduplication process (Astotin)
+#file=${sample_name}.dedup.bam ##changed to the trimmed sorted file because i dedup-ed using fastp
+file=SRR19221314.dedup.bam 
 
 echo "
      >>> Realigning TARGET for $file <<<

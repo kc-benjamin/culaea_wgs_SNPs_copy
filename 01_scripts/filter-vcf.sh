7f5dd8ec9678bc3cda3c19c655ef6d310d4b97ec
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-12 12:43:04 -0400

    added script to filter vcf file to just the amhy region

---
+++ 01_scripts/filter-vcf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name="filter-vcf"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=00-12:00:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --error=98_log_files/%x_%j.err
+
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh 
+conda activate /home/kcb95328/conda/envs/culaea_pkgs
+
+bcftools view --targets PGA_scaffold14__88_contigs__length_21401847:15,437,889-15,458,061 muir_snps_filtered.vcf.gz
+
+conda deactivate

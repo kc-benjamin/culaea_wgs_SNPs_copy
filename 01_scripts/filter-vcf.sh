4537b4a31f4c0fc404a207a83714a8a968055b8f
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-17 12:42:51 -0400

    moved input file behind options

--- 01_scripts/filter-vcf.sh
+++ 01_scripts/filter-vcf.sh
@@ -15,6 +15,6 @@ CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh 
 conda activate /home/kcb95328/conda/envs/culaea_pkgs
 
-bcftools view muir_snps_filtered.vcf.gz --regions PGA_scaffold14__88_contigs__length_21401847:15,437,889-15,458,061 > muir_snps_filtered_amhy.vcf
+bcftools view --regions PGA_scaffold14__88_contigs__length_21401847:15,437,889-15,458,061 muir_snps_filtered.vcf.gz > muir_snps_filtered_amhy.vcf
 
 conda deactivate

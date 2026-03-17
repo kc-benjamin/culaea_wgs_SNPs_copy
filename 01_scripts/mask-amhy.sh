d138239516566576f23eeeacaf15e1a6230063bd
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-17 17:07:48 -0400

    accidentally deleted

---
+++ 01_scripts/mask-amhy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name="filter-vcf"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --time=00-12:00:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --error=98_log_files/%x_%j.err
+
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh 
+conda activate /home/kcb95328/conda/envs/culaea_pkgs
+
+#bcftools index muir_snps_filtered.vcf.gz
+
+bcftools view -Ov -o muir_snps_filtered_amhy_biggerwindow2 -r PGA_scaffold14__88_contigs__length_21401847:15382674-15488296 muir_snps_filtered.vcf.gz
+
+conda deactivate

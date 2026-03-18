9b7cf63dc989935fad8981a57564f43daed9adce
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-18 12:02:42 -0400

    fixed reference input, fixed accidental deletion of both files

--- 01_scripts/mask-amhy.sh
+++ 01_scripts/mask-amhy.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 #SBATCH --partition=batch
-#SBATCH --job-name="filter-vcf"
+#SBATCH --job-name="mask-amhy"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
 #SBATCH --time=00-12:00:00
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=98_log_files/%x_%j.out
 #SBATCH --error=98_log_files/%x_%j.err
 
-CONDA_BASE=$(conda info --base)
-source ${CONDA_BASE}/etc/profile.d/conda.sh 
-conda activate /home/kcb95328/conda/envs/culaea_pkgs
+module load BEDTools/2.31.1-GCC-13.3.0 SAMtools/1.21-GCC-13.3.0
 
-#bcftools index muir_snps_filtered.vcf.gz
+#mask.bed file contents:
+#PGA_scaffold14__88_contigs__length_21401847   15445433       15450428
 
-bcftools view -Ov -o muir_snps_filtered_amhy_biggerwindow2 -r PGA_scaffold14__88_contigs__length_21401847:15382674-15488296 muir_snps_filtered.vcf.gz
+### mask Y par in reference fasta
+bedtools maskfasta \
+    -bed 03_genome/mask.bed \
+    -fi 03_genome/brook_genome_hap1_v1.fa \
+    -fo 03_genome/brook_genome_hap1_v1_amhy_masked.fa
 
-conda deactivate
+### index masked fasta
+samtools faidx 03_genome/brook_genome_hap1_v1_amhy_masked.fa

d26e16c386734edbc8fe759894523475727af3cd
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-19 12:14:35 -0400

    added script for genome alignment

---
+++ 01_scripts/genome_align.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name="genome_align"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=00-12:00:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --error=98_log_files/%x_%j.err
+
+module load MUMmer/4.0.1-GCCcore-13.3.0
+
+nucmer stickleback_v5_assembly.fa brook_genome_hap1_v1.fa

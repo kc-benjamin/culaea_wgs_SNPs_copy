8ccf22a7c39db70e6caace5dd042611d5a11f83c
kc-benjamin <katherine.benjamin@uga.edu>
2026-03-19 10:17:06 -0400

    changed to shunda (not masked)

--- 01_scripts/08_concat.sh
+++ 01_scripts/08_concat.sh
@@ -14,17 +14,17 @@
 module load VCFtools/0.1.16-GCC-13.3.0 BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCcore-13.3.0 
 #module intel/2020.1.217 
 
-vcf-concat $(ls -1 /scratch/kcb95328/AmhyMasked-Shunda/07_raw_VCFs/* | perl -pe 's/\n/ /g') > shunda_AmhyMasked_snps.vcf
+vcf-concat $(ls -1 /scratch/kcb95328/ShundaLakeBrooks/07_raw_VCFs/* | perl -pe 's/\n/ /g') > shunda_snps.vcf
 
-bgzip shunda_AmhyMasked_snps.vcf
-tabix --force --preset vcf shunda_AmhyMasked_snps.vcf
-bgzip --keep shunda_AmhyMasked_snps.vcf
+bgzip shunda_snps.vcf
+tabix --force --preset vcf shunda_snps.vcf
+bgzip --keep shunda_snps.vcf
 
-bcftools filter -e 'MQ < 30' shunda_AmhyMasked_snps.vcf.gz -Oz > tmp.vcf.gz
+bcftools filter -e 'MQ < 30' shunda_snps.vcf.gz -Oz > tmp.vcf.gz
 
-vcftools --gzvcf tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 5 --max-alleles 2 --recode --recode-INFO-all --stdout > shunda_AmhyMasked_snps_filtered.vcf
+vcftools --gzvcf tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 5 --max-alleles 2 --recode --recode-INFO-all --stdout > shunda_snps_filtered.vcf
 
 rm tmp.vcf.gz
 
-bgzip shunda_AmhyMasked_snps_filtered.vcf
-tabix -p vcf shunda_AmhyMasked_snps_filtered.vcf.gz
+bgzip shunda_snps_filtered.vcf
+tabix -p vcf shunda_snps_filtered.vcf.gz

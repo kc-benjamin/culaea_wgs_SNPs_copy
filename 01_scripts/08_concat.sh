8997b144cdbbea00ccb31036784d44b0d61d6717
kc-benjamin <katherine.benjamin@uga.edu>
2026-05-14 09:16:02 -0400

    changed for Shunda, which has already had the concat step done

--- 01_scripts/08_concat.sh
+++ 01_scripts/08_concat.sh
@@ -14,16 +14,16 @@
 module load VCFtools/0.1.16-GCC-13.3.0 BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCcore-13.3.0 
 #module intel/2020.1.217 
 
-vcf-concat $(ls -1 /scratch/kcb95328/AstotinLakeBrooks/07_raw_VCFs/* | perl -pe 's/\n/ /g') > Astotin_snps.vcf
+# vcf-concat $(ls -1 /scratch/kcb95328/AstotinLakeBrooks/07_raw_VCFs/* | perl -pe 's/\n/ /g') > Astotin_snps.vcf
 
-bgzip Astotin_snps.vcf
-tabix --force --preset vcf Astotin_snps.vcf
+# bgzip Astotin_snps.vcf
+# tabix --force --preset vcf Astotin_snps.vcf
 
-bcftools filter -e 'MQ < 30' Astotin_snps.vcf.gz -Oz > tmp.vcf.gz
+bcftools filter -e 'MQ < 30' Shunda_snps.vcf.gz -Oz > tmp.vcf.gz
 
-vcftools --gzvcf tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 1 --recode --recode-INFO-all --stdout > Astotin_snps_filtered.vcf
+vcftools --gzvcf tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 1 --recode --recode-INFO-all --stdout > Shunda_snps_filtered.vcf
 
 rm tmp.vcf.gz
 
-bgzip Astotin_snps_filtered.vcf
-tabix -p vcf Astotin_snps_filtered.vcf.gz
+bgzip Shunda_snps_filtered.vcf
+tabix -p vcf Shunda_snps_filtered.vcf.gz

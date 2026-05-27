efafeacbd47d0f08c9612bd7edfecb19c78da64c
kc-benjamin <katherine.benjamin@uga.edu>
2026-05-27 10:21:02 -0400

    changed to rerun on Shunda

--- 01_scripts/08a_rename_chr.sh
+++ 01_scripts/08a_rename_chr.sh
@@ -12,7 +12,7 @@
 #SBATCH --error=98_log_files/%x_%j.err
 
 ml BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCcore-13.3.0
-gunzip -c shunda_AmhyMasked_snps_filtered.vcf.gz > Shunda_AmhyMasked_snps_filtered.vcf
+#gunzip -c shunda_AmhyMasked_snps_filtered.vcf.gz > Shunda_AmhyMasked_snps_filtered.vcf
 
 awk '{gsub(/PGA_scaffold22__123_contigs__length_35631191/,"4"); print}' Shunda_AmhyMasked_snps_filtered.vcf > Shunda_AmhyMasked_snps_filtered_temp1.vcf
 awk '{gsub(/PGA_scaffold23__90_contigs__length_32925445/,"13"); print}' Shunda_AmhyMasked_snps_filtered_temp1.vcf > Shunda_AmhyMasked_snps_filtered_temp2.vcf
@@ -25,7 +25,7 @@ awk '{gsub(/PGA_scaffold14__88_contigs__length_21401847/,"20"); print}' Shunda_A
 awk '{gsub(/PGA_scaffold16__56_contigs__length_21394868/,"1"); print}' Shunda_AmhyMasked_snps_filtered_temp8.vcf > Shunda_AmhyMasked_snps_filtered_temp9.vcf
 awk '{gsub(/PGA_scaffold15__61_contigs__length_21329229/,"19"); print}' Shunda_AmhyMasked_snps_filtered_temp9.vcf > Shunda_AmhyMasked_snps_filtered_temp10.vcf
 awk '{gsub(/PGA_scaffold12__56_contigs__length_20552211/,"6"); print}' Shunda_AmhyMasked_snps_filtered_temp10.vcf > Shunda_AmhyMasked_snps_filtered_temp11.vcf
-awk '{gsub(/PGA_scaffold10__89_contigs__length_20417627/cd ,"16"); print}' Shunda_AmhyMasked_snps_filtered_temp11.vcf > Shunda_AmhyMasked_snps_filtered_temp12.vcf
+awk '{gsub(/PGA_scaffold10__89_contigs__length_20417627/,"16"); print}' Shunda_AmhyMasked_snps_filtered_temp11.vcf > Shunda_AmhyMasked_snps_filtered_temp12.vcf
 awk '{gsub(/PGA_scaffold13__42_contigs__length_19371112/,"7"); print}' Shunda_AmhyMasked_snps_filtered_temp12.vcf > Shunda_AmhyMasked_snps_filtered_temp13.vcf
 awk '{gsub(/PGA_scaffold11__71_contigs__length_19041060/,"11"); print}' Shunda_AmhyMasked_snps_filtered_temp13.vcf > Shunda_AmhyMasked_snps_filtered_temp14.vcf
 awk '{gsub(/PGA_scaffold7__70_contigs__length_18533579/,"15"); print}' Shunda_AmhyMasked_snps_filtered_temp14.vcf > Shunda_AmhyMasked_snps_filtered_temp15.vcf

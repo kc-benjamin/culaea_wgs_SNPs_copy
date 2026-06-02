353f3639d673309e13f2f2673f8e96663fa349e0
kc-benjamin <katherine.benjamin@uga.edu>
2026-06-02 14:56:46 -0400

    increased resources, added command to remove tmp file at the beginning

--- 01_scripts/star-align.sh
+++ 01_scripts/star-align.sh
@@ -4,9 +4,9 @@
 #SBATCH --job-name="star-align"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --time=06:00:00
-#SBATCH --mem=1G
+#SBATCH --mem=8G
 #SBATCH --mail-user=kcb95328@uga.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=99_log_files/star-align_%j.out
@@ -14,6 +14,7 @@
 #SBATCH --array=1-40
 
 ml STAR/2.7.11a-GCC-12.3.0
+rm -r _STARtmp
 
 #variables
 INDIR="01_raw_reads"
@@ -26,13 +27,13 @@ samp_num=$SLURM_ARRAY_TASK_ID
 # Pull sample name from the sample info
 name=$(cut -f1 raw_data_list3.txt | sed -n "${samp_num}p")
 
-STAR --runThreadN 4 \
+STAR --runThreadN $SLURM_CPUS_PER_TASK \
      --runMode genomeGenerate \
      --genomeDir $GENOMEDIR \
      --genomeFastaFiles $SLURM_SUBMIT_DIR/brook_genome_hap1_v1.fasta \
      --sjdbGTFfile $SLURM_SUBMIT_DIR/brook_genome_hap1_v1.gff \
 
-STAR --runThreadN 4 \
+STAR --runThreadN $SLURM_CPUS_PER_TASK \
      --genomeDir $GENOMEDIR \
      --readFilesIn $INDIR/${name}_R1_001.fastq.gz $INDIR/${name}_R2_001.fastq.gz \
      --readFilesCommand zcat \

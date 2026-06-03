89ed7542d6785c59b695beddb08aa5b7bd908193
kc-benjamin <katherine.benjamin@uga.edu>
2026-06-03 09:26:44 -0400

    edited to rerun on Muir

--- 01_scripts/08_concat.sh
+++ 01_scripts/08_concat.sh
@@ -15,16 +15,16 @@ module load VCFtools/0.1.16-GCC-13.3.0 BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCc
 #module intel/2020.1.217 
 OUT="/scratch/kcb95328/MuirLakeBrooks/08_PLINK_new"
 
-vcf-concat $(ls -1 /scratch/kcb95328/MuirLakeBrooks/07_vcfs_new/* | perl -pe 's/\n/ /g') > $OUT/Muir_snps.vcf
+vcf-concat $(ls -1 /scratch/kcb95328/MuirLakeBrooks/07_vcfs_new/* | perl -pe 's/\n/ /g') > $OUT/Muir_snps_ALL.vcf
 
-bgzip $OUT/Muir_snps.vcf
-tabix --force --preset vcf $OUT/Muir_snps.vcf
+bgzip $OUT/Muir_snps_ALL.vcf
+tabix --force --preset vcf $OUT/Muir_snps_ALL.vcf
 
-bcftools filter -e 'MQ < 30' $OUT/Muir_snps.vcf.gz -Oz > $OUT/tmp.vcf.gz
+bcftools filter -e 'MQ < 30' $OUT/Muir_snps_ALL.vcf.gz -Oz > $OUT/tmp.vcf.gz
 
-vcftools --gzvcf $OUT/tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 1 --recode --recode-INFO-all --stdout > $OUT/Muir_snps_filtered.vcf
+vcftools --gzvcf $OUT/tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 1 --recode --recode-INFO-all --stdout > $OUT/Muir_snps_ALL_filtered.vcf
 
 rm tmp.vcf.gz
 
-bgzip $OUT/Muir_snps_filtered.vcf
-tabix -p vcf $OUT/Muir_snps_filtered.vcf.gz
+bgzip $OUT/Muir_snps_ALL_filtered.vcf
+tabix -p vcf $OUT/Muir_snps_ALL_filtered.vcf.gz

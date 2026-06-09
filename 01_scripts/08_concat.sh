90370c119d18f6519adf3663ab6f4df797cebb3c
kc-benjamin <katherine.benjamin@uga.edu>
2026-06-09 10:31:56 -0400

    changed to run on the increased filtering data in astotin

--- 01_scripts/08_concat.sh
+++ 01_scripts/08_concat.sh
@@ -12,19 +12,24 @@
 #SBATCH --error=98_log_files/%x_%j.err
 
 module load VCFtools/0.1.16-GCC-13.3.0 BCFtools/1.21-GCC-13.3.0 tabix/0.2.6-GCCcore-13.3.0 
-#module intel/2020.1.217 
-OUT="/scratch/kcb95328/MuirLakeBrooks/08_PLINK_new"
+#module intel/2020.1.217
+DIR="/scratch/kcb95328/IncreasedFiltering-Astotin"
+VCFS="/07_vcfs"
+PLINK="/08_PLINK"
 
-vcf-concat $(ls -1 /scratch/kcb95328/MuirLakeBrooks/07_vcfs_new/* | perl -pe 's/\n/ /g') > $OUT/Muir_snps_ALL.vcf
+vcf-concat $(ls -1 ${DIR}/${VCFS}/* | perl -pe 's/\n/ /g') > $DIR/$PLINK/Ast_snps_IncFilt.vcf
 
-bgzip $OUT/Muir_snps_ALL.vcf
-tabix --force --preset vcf $OUT/Muir_snps_ALL.vcf
+bgzip $DIR/$PLINK/Ast_snps_IncFilt.vcf
+tabix --force --preset vcf $DIR/$PLINK/Ast_snps_IncFilt.vcf
 
-bcftools filter -e 'MQ < 30' $OUT/Muir_snps_ALL.vcf.gz -Oz > $OUT/tmp.vcf.gz
+bcftools filter -e 'MQ < 30' $DIR/$PLINK/Ast_snps_IncFilt.vcf.gz -Oz > $DIR/$PLINK/tmp.vcf.gz
+#mapping quality is 30 here--bams only align in the "IncreasedFiltering" folders if MQ > 20
 
-vcftools --gzvcf $OUT/tmp.vcf.gz --minQ 30 --minGQ 20 --minDP 1 --recode --recode-INFO-all --stdout > $OUT/Muir_snps_ALL_filtered.vcf
+vcftools --gzvcf $DIR/$PLINK/tmp.vcf.gz --minQ 30 --minGQ 30 --minDP 1 --recode --recode-INFO-all --stdout > $DIR/$PLINK/Ast_snps_IncFilt_filtered.vcf
+#from Jon: minimum quality is 30, minimum genotype quality is 20, and minimum depth is 1
+#I increased the GQ (confidence that a genotype call is correct) to 30
 
-rm tmp.vcf.gz
+rm $DIR/$PLINK/tmp.vcf.gz
 
-bgzip $OUT/Muir_snps_ALL_filtered.vcf
-tabix -p vcf $OUT/Muir_snps_ALL_filtered.vcf.gz
+bgzip $DIR/$PLINK/Ast_snps_IncFilt_filtered.vcf
+tabix -p vcf $DIR/$PLINK/Ast_snps_IncFilt_filtered.vcf.gz

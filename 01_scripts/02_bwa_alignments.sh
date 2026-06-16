86e351008a5d60d9fb015a98039708a08f996c16
kc-benjamin <katherine.benjamin@uga.edu>
2026-06-16 09:32:01 -0400

    changed global variables to reflect new file structure; for Muir

--- 01_scripts/02_bwa_alignments.sh
+++ 01_scripts/02_bwa_alignments.sh
@@ -14,19 +14,19 @@
 
 #PREFIX=$(sed -n "${SLURM_ARRAY_TASK_ID}p" 02_info_files/SRR_Acc_List_ML.txt)
 # Load needed modules
-#for Astotin
 CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh 
 conda activate /home/kcb95328/conda/envs/culaea_pkgs
 
 # Global variables
-GENOMEFOLDER="03_genome"
-GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fa | xargs -n 1 basename)
+POPULATION="/home/kcb95328/Info-Muir"
+
+GENOMEFOLDER="/home/kcb95328/genomes"
+GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fasta | xargs -n 1 basename)
 GENOME_FULL="$GENOMEFOLDER/$GENOME"
-##INDGENOME="${GENOME}.fai"
-RAWDATAFOLDER="05_trimmed_new"
-ALIGNEDFOLDER="06_aligned_bams"
-#ALIGNED_test="06_bam_files/test3"
+
+RAWDATAFOLDER="/scratch/kcb95328/MuirLakeBrooks/05_trimmed_new"
+ALIGNEDFOLDER="01_aligned_bams"
 LOG_FOLDER="98_log_files"
 #echo "$GENOME and $INDGENOME found in $GENOMEFOLDER"
 
@@ -43,7 +43,7 @@ echo "samp_num='$samp_num'"
 #echo "PREFIX='$PREFIX'"
 
 # Pull sample name from the sample info
-name=$(cut -f1 02_info_files/SRR_Acc_List_ML.txt | sed -n "${samp_num}p")
+name=$(cut -f1 $POPULATION/SRR_Acc_List_ML.txt | sed -n "${samp_num}p")
 #name=SRR19221338
 
 # Name of uncompressed file
@@ -60,7 +60,7 @@ echo $RG
 # Align reads
 #bwa index $GENOME_FULL bwa-generated-index
 bwa mem -t $NCPU -R $RG $GENOME_FULL $RAWDATAFOLDER/$file1 $RAWDATAFOLDER/$file2 |
-    samtools view -b -q 20 -o "$ALIGNEDFOLDER/${name}.bam" #previously used -q 10 to generate first sets of files
+    samtools view -b -o "$ALIGNEDFOLDER/${name}.bam" #previously used -q 10 to generate first sets of files (or -q 20 for the Increased Filtering files)
 
 # Sort
 samtools sort -@ $NCPU $ALIGNEDFOLDER/${name}.bam \

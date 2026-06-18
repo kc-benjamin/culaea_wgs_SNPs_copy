a1c6414cfecfade8f633bc90182eda043eb06198
kc-benjamin <katherine.benjamin@uga.edu>
2026-06-18 11:36:59 -0400

    changed to align the muir files to the masked genome file

--- 01_scripts/02_bwa_alignments.sh
+++ 01_scripts/02_bwa_alignments.sh
@@ -19,14 +19,14 @@ source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate /home/kcb95328/conda/envs/culaea_pkgs
 
 # Global variables
-POPULATION="/home/kcb95328/Info-Shunda"
+POPULATION="/home/kcb95328/Info-Muir" #change this line as needed
 
 GENOMEFOLDER="/home/kcb95328/genomes"
-GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1.fasta | xargs -n 1 basename)
+GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1_amhy_masked.fa | xargs -n 1 basename)
 GENOME_FULL="$GENOMEFOLDER/$GENOME"
 
-RAWDATAFOLDER="/scratch/kcb95328/ShundaLakeBrooks/05_trimmed_data"
-ALIGNEDFOLDER="01_aligned_bams"
+RAWDATAFOLDER="/scratch/kcb95328/MuirLakeBrooks/05_trimmed_data" #change as needed to the main population directory
+ALIGNEDFOLDER="01_aligned_bams" #change as needed; should be from the submit directory
 LOG_FOLDER="98_log_files"
 #echo "$GENOME and $INDGENOME found in $GENOMEFOLDER"
 
@@ -43,7 +43,7 @@ echo "samp_num='$samp_num'"
 #echo "PREFIX='$PREFIX'"
 
 # Pull sample name from the sample info
-name=$(cut -f1 $POPULATION/SRR_Acc_List_SL.txt | sed -n "${samp_num}p")
+name=$(cut -f1 $POPULATION/SRR_Acc_List_ML.txt | sed -n "${samp_num}p") #change as needed
 #name=SRR19221338
 
 # Name of uncompressed file

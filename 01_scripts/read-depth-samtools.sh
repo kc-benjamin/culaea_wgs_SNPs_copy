5ec5f931fce57593663b3980f53c13493d978314
kc-benjamin <katherine.benjamin@uga.edu>
2026-07-06 15:07:19 -0400

    creation of script for samtools read depth

---
+++ 01_scripts/read-depth-samtools.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name="read_depth_samtools"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --time=00-12:00:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=98_log_files/%x_%j.out
+#SBATCH --error=98_log_files/%x_%j.err
+#SBATCH --array=1-95
+
+# Load needed modules
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh 
+conda activate /home/kcb95328/conda/envs/culaea_pkgs
+
+# Global variables
+POPULATION="/home/kcb95328/Info-Shunda" #change this line as needed
+GENOMEFOLDER="/home/kcb95328/genomes"
+GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1_amhy_masked.fa | xargs -n 1 basename)
+GENOME_FULL="$GENOMEFOLDER/$GENOME"
+
+DATAFOLDER="/scratch/kcb95328/AmhyMasked-Shunda/Unfiltered/01_aligned_bams" #change as needed to the main population directory
+#OUTPUT= #change as needed; should be from the submit directory
+LOG_FOLDER="98_log_files"
+name=$(cut -f1 $POPULATION/SRR_Acc_List_SL.txt | sed -n "${SLURM_ARRAY_TASK_ID}p") #change as needed
+
+
+#Calculate coverage for a specific chromosome:
+samtools coverage -r PGA_scaffold14__88_contigs__length_21401847 -m -w 10000 ${name}.bam > ${name}_chr20_coverage.txt
+
+conda deactivate

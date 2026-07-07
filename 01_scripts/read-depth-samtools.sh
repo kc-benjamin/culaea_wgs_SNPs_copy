a416052eccca55b0e5538d2294f3dd991a2a21ea
kc-benjamin <katherine.benjamin@uga.edu>
2026-07-07 11:52:58 -0400

    edited for cleanliness; updated to the no-filtering population

--- 01_scripts/read-depth-samtools.sh
+++ 01_scripts/read-depth-samtools.sh
@@ -18,21 +18,16 @@ conda activate /home/kcb95328/conda/envs/culaea_pkgs
 
 # Global variables
 POPULATION="/home/kcb95328/Info-Shunda" #change this line as needed
-GENOMEFOLDER="/home/kcb95328/genomes"
-GENOME=$(ls -1 $GENOMEFOLDER/brook_genome_hap1_v1_amhy_masked.fa | xargs -n 1 basename)
-GENOME_FULL="$GENOMEFOLDER/$GENOME"
 
-DATAFOLDER="/scratch/kcb95328/ShundaLakeBrooks/06_bam_files" #change as needed to the main population directory
-OUTPUT="/scratch/kcb95328/ShundaLakeBrooks/11_read_depth" #change as needed; should be from the submit directory
-LOG_FOLDER="98_log_files"
-name=$(cut -f1 $POPULATION/SRR_Acc_List_SL.txt | sed -n "${SLURM_ARRAY_TASK_ID}p") #change as needed
+OUTPUT="/scratch/kcb95328/NoFiltering-Shunda" #change as needed; should be from the submit directory
+#name=$(cut -f1 $POPULATION/SRR_Acc_List_SL.txt | sed -n "${SLURM_ARRAY_TASK_ID}p") #change as needed
 
 
 #Calculate coverage for a specific chromosome viewable at the terminal level, requires no secondary analysis but is per-file:
 #samtools coverage -r PGA_scaffold14__88_contigs__length_21401847 -w 113 --plot-depth --ascii $DATAFOLDER/${name}.trimmed.fastq.gz.sorted.bam > ${OUTPUT}/${name}_chr20_coverage.txt
 
 #Calculate coverage for a chromosome to be used with R:
-samtools depth -a -H -J -r PGA_scaffold14__88_contigs__length_21401847 -f $POPULATION/SL_bamfiles_complete_name.txt -o $OUTPUT/Shunda-all-chr20-depth.txt
+samtools depth -a -H -J -r "PGA_scaffold14__88_contigs__length_21401847" -f $POPULATION/SL_bamfiles_complete_name.txt -o $OUTPUT/Shunda-all-chr20-depth.txt
 #-a: output all positions, including those with zero coverage
 #-H: show column names at the beginning of the output
 #-J: include reads with deletions in depth computation

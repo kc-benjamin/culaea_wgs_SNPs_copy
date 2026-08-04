8e9ffb3657f6b5fd566673bb586b924a2b483285
kc-benjamin <katherine.benjamin@uga.edu>
2026-08-04 10:25:04 -0400

    scripts for mapping read depth (Shunda)

---
+++ 01_scripts/read-depth-plot.r
@@ -0,0 +1,60 @@
+install.packages(c("zoo", "ggplot2", "tidyr", "dplyr", "rollmean"))
+
+library('ggplot2')
+library('tidyr')
+library('zoo')
+library('rollmean')
+library('dplyr')
+setwd("/scratch/kcb95328/ShundaLakeBrooks/11_read_depth")
+
+#read in data
+df <- read.delim("Shunda-11to19-chr20-depth.txt")
+
+#reformat data
+colnames(df) <- gsub("X.scratch.kcb95328.ShundaLakeBrooks.09_bam_copies."," ",colnames(df)) 
+names(df) <- gsub(".trimmed.fastq.gz.sorted.bam"," ",names(df))
+colnames(df) <- trimws(colnames(df))
+
+#reformat for combination with sex
+depth_long<-pivot_longer(df,cols=starts_with("SRR"))
+depth_long$POS<-as.numeric(depth_long$POS)
+depth_long$value<-as.numeric(depth_long$value)
+
+#read in sex metadata
+sex_shunda<-read.csv("/home/kcb95328/Info-Shunda/SraRunTable-metadata-SL.csv", header=TRUE)
+sex_shunda<-sex_shunda[, c("Run","sex")]
+
+#normalize depth values
+depth_long$value <- depth_long$value / mean(depth_long$value)
+
+#merge
+names(depth_long)[3]<-"Run"
+df_with_sex<-merge(depth_long,sex_shunda, by="Run")
+
+#smooth out the data:
+window_size <- 10000
+
+df_with_sex$smooth <- rollmean(df_with_sex$value, k = window_size, fill = NA, align = "center")
+
+#plot
+largeplot<-ggplot(df_with_sex, aes(x = POS)) +
+  geom_line(aes(y = value), color = "gray80", alpha = 0.5) +  # Raw data in the background
+  geom_line(aes(y = smooth), color = "blue", size = 1) + # Smoothed line
+  labs(title = "Sliding Window Smoothing (Rolling Mean)", x = "Position (bp)", y = "Depth")
+#save it
+ggsave(filename="Norm10000-Shunda-chr20-11to19-all_8-2026.pdf", plot=largeplot)
+
+#let's get rid of that crazy region:
+#define threshold
+threshold <- 3
+#keep only those below the threshold
+small_df_with_sex<- df_with_sex %>%
+    filter(value <= threshold)
+
+#plot
+thresholdplot<-ggplot(small_df_with_sex, aes(x = POS)) +
+  geom_line(aes(y = smooth), color = "blue", size = 1) + # Smoothed line
+  labs(title = "Sliding Window Smoothing (Rolling Mean)", x = "Position (bp)", y = "Depth") +
+  theme(text = element_text(family = "Arial"))
+#save
+ggsave(filename="Threshold3-Norm10000-Shunda-chr20-11to19-all_8-2026.pdf", plot=thresholdplot)
---
+++ 01_scripts/read-depth-plot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=highmem_p
+#SBATCH --job-name="read_depth_R"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=300G
+#SBATCH --time=0-24:00:00
+#SBATCH --mail-user=kcb95328@uga.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=/scratch/kcb95328/ShundaLakeBrooks/98_log_files/read_depth_R_%j.out
+#SBATCH --error=/scratch/kcb95328/ShundaLakeBrooks/98_log_files/read_depth_R_%j.err
+
+#go to submit directory
+cd $SLURM_SUBMIT_DIR
+
+#activate R script
+R --no-save < /home/kcb95328/culaea_wgs_SNPs_copy/01_scripts/read-depth-plot.r

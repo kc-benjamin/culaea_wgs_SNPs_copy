e14c06f426af4bf7bde826307479f7eab4dd26a7
kc-benjamin <katherine.benjamin@uga.edu>
2026-08-07 09:06:43 -0400

    edited loop to produce the correct graphs

--- 01_scripts/read-depth-plot.r
+++ 01_scripts/read-depth-plot.r
@@ -22,35 +22,40 @@ sex_shunda <- sex_shunda[, c("Run", "sex")]
 for(i in sex_shunda$Run) {
   df <- read.delim(paste0(i, "_chr20_11.7to19.2_read-depth.txt"), header = TRUE)
 
+  infile <- paste0(i, "_chr20_11.7to19.2_read-depth.txt")
+  message("Reading: ", infile)
 
-#reformat data
-  colnames(df) <- gsub("X.scratch.kcb95328.ShundaLakeBrooks.09_bam_copies.", " ", colnames(df))
-  names(df) <- gsub(".trimmed.fastq.gz.sorted.bam", " ", names(df))
+  #reformat data
+  colnames(df) <- gsub("X.scratch.kcb95328.ShundaLakeBrooks.06_bam_files.", " ", colnames(df))
+  names(df) <- gsub(".trimmed.fastq.gz.sorted.bam.bai", " ", names(df))
   colnames(df) <- trimws(colnames(df))
 
-#reformat for combination with sex
+  #reformat for combination with sex
   depth_long <- pivot_longer(df, cols = starts_with("SRR"))
   depth_long$POS <- as.numeric(depth_long$POS)
   depth_long$value <- as.numeric(depth_long$value)
   rm(df)
 
-#normalize depth values
+  #normalize depth values
   depth_long$value <- depth_long$value / mean(depth_long$value)
 
-#merge
+  #merge
   names(depth_long)[3] <- "Run"
   df_with_sex <- merge(depth_long,sex_shunda, by = "Run")
 
-#smooth out the data:
+  #smooth out the data:
   window_size <- 10000
 
   df_with_sex$smooth <- rollmean(df_with_sex$value, k = window_size, fill = NA, align = "center")
 
-#plot (males only)
+  #plot (males only)
   plot <- ggplot(df_with_sex, aes(x = POS)) +
     geom_line(aes(y = smooth), color = "blue", linewidth = 1) + # Smoothed line
-    labs(title = "Read depth from 11M to 19M- Normalized 10,000 bp windows", x = "Position (bp)", y = "Depth")
-#save it
+    labs(
+      title = paste("Read depth from 11M to 19M", i, "Normalized 10,000 bp windows"),
+      x = "Position (bp)",
+      y = "Depth")
+  #save it
   pdf(paste0("Norm10000-Shunda-chr20-11to19", i, ".pdf"))
   print(plot)
   dev.off()

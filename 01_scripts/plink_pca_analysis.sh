56e807178136d4c405a0c464b39387b733718396
kc-benjamin <katherine.benjamin@uga.edu>
2026-09-15 13:03:55 -0400

    analysis for Muir

--- 01_scripts/plink_pca_analysis.sh
+++ 01_scripts/plink_pca_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=batch
-#SBATCH --job-name="plink_pca_analysis_Astotin"
+#SBATCH --job-name="plink_pca_analysis_Muir"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
@@ -18,12 +18,12 @@ conda activate /home/kcb95328/conda/envs/culaea_pkgs
 
 #this code assumes you have already generated the filtered, renamed, maf and genotype files
 cd $SLURM_SUBMIT_DIR #should be the same as your input folder
-INPUT="/scratch/kcb95328/AstotinLakeBrooks/"
-DATA="/home/kcb95328/Info-Astotin/"
+INPUT="/scratch/kcb95328/MuirLakeBrooks/"
+DATA="/home/kcb95328/Info-Muir/"
 
 
-plink --file Astotin_snps_geno20_maf01 --pca 10 --out Astotin_pca_out
-plink --file Astotin_snps_geno20_maf01 --logistic mperm=10000 --allow-extra-chr --allow-no-sex --covar Astotin_pca_out.eigenvec --covar-number 1-4 --pheno $DATA/AS_sex_pheno_numbers.txt --mpheno 1 --out Astotin_with_pca_GWAS_results
-#plink --file Astotin_snps_geno20_maf01 --logistic mperm=10000 --allow-extra-chr --allow-no-sex --covar Astotin_pca_out.eigenvec --covar-number 1-4 --pheno $DATA/AL_pheno_numbers_all.txt --mpheno 1 --out Astotin_with_pca_GWAS_results
+plink --file Muir_snps_geno20_maf01 --pca 10 --out Muir_pca_out
+plink --file Muir_snps_geno20_maf01 --logistic mperm=10000 --allow-extra-chr --allow-no-sex --covar Muir_pca_out.eigenvec --covar-number 1-4 --pheno $DATA/ML_all_phenotypes.txt --mpheno 1 --out Muir_with_pca_GWAS_results
+#plink --file Muir_snps_geno20_maf01 --logistic mperm=10000 --allow-extra-chr --allow-no-sex --covar Muir_pca_out.eigenvec --covar-number 1-4 --pheno $DATA/AL_pheno_numbers_all.txt --mpheno 1 --out Muir_with_pca_GWAS_results
 
 conda deactivate
